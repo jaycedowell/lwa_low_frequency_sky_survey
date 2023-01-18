175451d5f1b634f787b41b4f736ae887e0fbab8c
jaycedowell <jaycedowell@gmail.com>
2023-01-18 15:55:00 -0700

    Initial scripts.

---
+++ image/run16.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+filename=${1}
+outname=`basename ${filename} | sed -e 's/\..*//g;' `
+outname=`echo "${outname}_16MHz" `
+
+#wsclean -size 350 350 -scale 20amin -niter 10000 -weight natural -mgain 0.8 -nonegative -joinpolarizations -pol xx,yy -channelrange 308 318 -no-update-model-required -name ${outname} ${filename}
+
+isSim=`basename ${filename} | grep _SIM `
+if [[ "${isSim}" == "" ]]; then
+	threshold=26.4
+else
+	threshold=`echo "2.25*1658.9" | bc -l `
+fi
+
+shmDir=`mktemp --tmpdir=/dev/shm -d wsclean.XXXXXX `
+cp -r ${filename} ${shmDir}
+shmname=`basename ${filename} | xargs -n1 -i{} echo "${shmDir}/{}" `
+
+wsclean -size 350 350 -scale 20amin -niter 75000 -threshold ${threshold} -weight natural -multiscale -multiscale-scale-bias 0.9 -mgain 0.6 -nonegative -joinpolarizations -pol xx,yy -joinchannels -channelrange 140 180 -no-update-model-required -fitsmask mask_350.fits -name ${outname} ${shmname} > ${outname}.log 2>&1
+
+rm -rf ${shmDir}
---
+++ image/runMulti2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+filename=${1}
+outname=`basename ${filename} | sed -e 's/\..*//g;' `
+outname=`echo "${outname}_Multi" `
+
+wsclean -size 350 350 -scale 20amin -niter 10000 -weight briggs 0.5 -multiscale -multiscale-threshold-bias 0.8 -mgain 0.8 -nonegative -joinpolarizations -pol xx,yy -channelrange 300 1140 -joinchannels -no-update-model-required -fitsmask mask_350.fits -name ${outname} ${filename}
