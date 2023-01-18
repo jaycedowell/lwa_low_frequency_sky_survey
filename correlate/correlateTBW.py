ecef382c7186f3168e009786669526ebf64b70c0
jaycedowell <jaycedowell@gmail.com>
2023-01-18 16:01:26 -0700

    Update to the newest version from LSL.

--- correlate/correlateTBW.py
+++ correlate/correlateTBW.py
@@ -1,123 +1,44 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
 """
 Example script that reads in TBW data and runs a cross-correlation on it.  
 The results are saved in the FITS IDI format.
 """
 
+# Python2 compatibility
+from __future__ import print_function, division, absolute_import
+import sys
+if sys.version_info < (3,):
+    range = xrange
+    
 import os
 import sys
 import time
-import ephem
 import numpy
-import getopt
-from datetime import datetime, timedelta, tzinfo
+import argparse
 
-from lsl import astro
-from lsl.reader.ldp import LWA1DataFile
+from lsl.reader.ldp import LWA1DataFile, TBWFile
 from lsl.common import stations, metabundle
 from lsl.correlator import fx as fxc
-from lsl.writer import measurementset as fitsidi
+from lsl.writer import fitsidi, measurementset
 from lsl.common.progress import ProgressBar
+from lsl.misc import parser as aph
+
+from lsl.misc import telemetry
+telemetry.track_script()
 
 
-class UTC(tzinfo):
-    """tzinfo object for UTC time."""
-    
-    def utcoffset(self, dt):
-        return timedelta(0)
-        
-    def tzname(self, dt):
-        return "UTC"
-        
-    def dst(self, dt):
-        return timedelta(0)
-
-
-def usage(exitCode=None):
-    print """correlateTBW.py - cross-correlate data in a TBW file
-
-Usage: correlateTBW.py [OPTIONS] file
-
-Options:
--h, --help             Display this help information
--m, --metadata         Name of SSMIF or metadata tarball file to use for 
-                    mappings
--l, --fft-length       Set FFT length (default = 2048)
--q, --quiet            Run correlateTBW in silent mode
--a, --all              Correlated all dipoles regardless of their status 
-                    (default = no)
--x, --xx               Compute only the XX polarization product (default)
--y, --yy               Compute only the YY polarization product
--2, --two-products     Compute both the XX and YY polarization products
-"""
-    
-    if exitCode is not None:
-        sys.exit(exitCode)
-    else:
-        return True
-
-
-def parseConfig(args):
-    config = {}
-    # Command line flags - default values
-    config['metadata'] = ''
-    config['LFFT'] = 2048
-    config['samples'] = 1
-    config['offset'] = 0
-    config['verbose'] = True
-    config['all'] = False
-    config['products'] = ['xx',]
-    config['args'] = []
-    
-    # Read in and process the command line flags
-    try:
-        opts, arg = getopt.getopt(args, "hm:ql:axy2", ["help", "metadata=", "quiet", "fft-length=", "all", "xx", "yy", "two-products"])
-    except getopt.GetoptError, err:
-        # Print help information and exit:
-        print str(err) # will print something like "option -a not recognized"
-        usage(exitCode=2)
-        
-    # Work through opts
-    for opt, value in opts:
-        if opt in ('-h', '--help'):
-            usage(exitCode=0)
-        elif opt in ('-m', '--metadata'):
-            config['metadata'] = value
-        elif opt in ('-q', '--quiet'):
-            config['verbose'] = False
-        elif opt in ('-l', '--fft-length'):
-            config['LFFT'] = int(value)
-        elif opt in ('-a', '--all'):
-            config['all'] = True
-        elif opt in ('-x', '--xx'):
-            config['products'] = ['xx',]
-        elif opt in ('-y', '--yy'):
-            config['products'] = ['yy',]
-        elif opt in ('-2', '--two-products'):
-            config['products'] = ['xx', 'yy']
-        else:
-            assert False
-            
-    # Add in arguments
-    config['args'] = arg
-    
-    # Return configuration
-    return config
-
-
-def processChunk(idf, site, good, filename, LFFT=64, Overlap=1, pols=['xx','yy']):
+def process_chunk(idf, site, good, filename, LFFT=64, overlap=1, pfb=False, pols=['xx','yy']):
     """
     Given an lsl.reader.ldp.TBWFile instances and various parameters for the 
     cross-correlation, write cross-correlate the data and save it to a file.
     """
     
     # Get antennas
-    antennas = site.getAntennas()
+    antennas = site.antennas
     
     # Get the metadata
-    sampleRate = idf.getInfo('sampleRate')
+    sample_rate = idf.get_info('sample_rate')
     
     # Create the list of good digitizers and a digitizer to Antenna instance mapping.  
     # These are:
@@ -129,24 +50,33 @@ def processChunk(idf, site, good, filename, LFFT=64, Overlap=1, pols=['xx','yy']
     # Create a list of unqiue stands to know what style of IDI file to create
     stands = set( [antennas[i].stand.id for i in good] )
     
+    # Figure out the output mode
+    if os.path.splitext(filename)[1].find('.ms_') != -1:
+        writer_class = measurementset.Ms
+    else:
+        if len(stands) > 255:
+            writer_class = fitsidi.ExtendedIdi
+        else:
+            writer_class = fitsidi.Idi
+            
     wallTime = time.time()
     readT, t, data = idf.read()
     setTime = t
-    refTime = t
+    ref_time = t
     
-    setDT = datetime.utcfromtimestamp(setTime)
-    setDT.replace(tzinfo=UTC())
-    print "Working on set #1 (%.3f seconds after set #1 = %s)" % ((setTime-refTime), setDT.strftime("%Y/%m/%d %H:%M:%S.%f"))
+    # Setup the set time as a python datetime instance so that it can be easily printed
+    setDT = setTime.datetime
+    print("Working on set #1 (%.3f seconds after set #1 = %s)" % ((setTime-ref_time), setDT.strftime("%Y/%m/%d %H:%M:%S.%f")))
     
     # In order for the TBW stuff to actaully run, we need to run in with sub-
     # integrations.  8 sub-integrations (61.2 ms / 8 = 7.7 ms per section) 
     # seems to work ok with a "reasonable" number of channels.
     nSec = 8
-    secSize = data.shape[1]/nSec
+    secSize = data.shape[1]//nSec
     
     # Loop over polarizations (there should be only 1)
     for pol in pols:
-        print "-> %s" % pol
+        print("-> %s" % pol)
         try:
             tempVis *= 0    # pylint:disable=undefined-variable
         except NameError:
@@ -159,15 +89,15 @@ def processChunk(idf, site, good, filename, LFFT=64, Overlap=1, pols=['xx','yy']
         sys.stdout.flush()
         
         # Loop over sub-integrations (set by nSec)
-        for k in xrange(nSec):
-            blList, freq, vis = fxc.FXMaster(data[toKeep,k*secSize:(k+1)*secSize], mapper, LFFT=LFFT, Overlap=Overlap, IncludeAuto=True, verbose=False, SampleRate=sampleRate, CentralFreq=0.0, Pol=pol, ReturnBaselines=True, GainCorrect=True)
+        for k in range(nSec):
+            blList, freq, vis = fxc.FXMaster(data[toKeep,k*secSize:(k+1)*secSize], mapper, LFFT=LFFT, overlap=overlap, pfb=pfb, include_auto=True, verbose=False, sample_rate=sample_rate, central_freq=0.0, pol=pol, return_baselines=True, gain_correct=True)
             
-            toUse = numpy.where( (freq>=12.0e6) & (freq<=86.0e6) )
+            toUse = numpy.where( (freq>=5.0e6) & (freq<=93.0e6) )
             toUse = toUse[0]
             
             try:
                 tempVis += vis
-            except:
+            except NameError:
                 tempVis = vis
                 
             pb.inc(amount=1)
@@ -179,20 +109,19 @@ def processChunk(idf, site, good, filename, LFFT=64, Overlap=1, pols=['xx','yy']
         
         # Set up the FITS IDI file is we need to
         if pol == pols[0]:
-            pol1, pol2 = fxc.pol2pol(pol)
+            pol1, pol2 = fxc.pol_to_pols(pol)
             
-            fits = fitsidi.MS(filename, refTime=refTime)
-            fits.setStokes(pols)
-            fits.setFrequency(freq[toUse])
-            fits.setGeometry(site, [a for a in mapper if a.pol == pol1])
+            fits = writer_class(filename, ref_time=ref_time)
+            fits.set_stokes(pols)
+            fits.set_frequency(freq[toUse])
+            fits.set_geometry(site, [a for a in mapper if a.pol == pol1])
             
         # Add the visibilities
-        obsTime = astro.unix_to_taimjd(setTime)
-        fits.addDataSet(obsTime, readT, blList, vis[:,toUse], pol=pol)
+        fits.add_data_set(setTime, readT, blList, vis[:,toUse], pol=pol)
         sys.stdout.write(pb.show()+'\r')
         sys.stdout.write('\n')
         sys.stdout.flush()
-    print "->  Cummulative Wall Time: %.3f s (%.3f s per integration)" % ((time.time()-wallTime), (time.time()-wallTime))
+    print("->  Cummulative Wall Time: %.3f s (%.3f s per integration)" % ((time.time()-wallTime), (time.time()-wallTime)))
     
     fits.write()
     fits.close()
@@ -204,37 +133,33 @@ def processChunk(idf, site, good, filename, LFFT=64, Overlap=1, pols=['xx','yy']
 
 def main(args):
     # Parse command line options
-    config = parseConfig(args)
-    filename = config['args'][0]
-
-    # Length of the FFT
-    LFFT = config['LFFT']
+    filename = args.filename
 
     # Setup the LWA station information
-    if config['metadata'] != '':
+    if args.metadata is not None:
         try:
-            station = stations.parseSSMIF(config['metadata'])
+            station = stations.parse_ssmif(args.metadata)
         except ValueError:
-            station = metabundle.getStation(config['metadata'], ApplySDM=True)
+            station = metabundle.get_station(args.metadata, apply_sdm=True)
     else:
         station = stations.lwa1
-    antennas = station.getAntennas()
+    antennas = station.antennas
     
     idf = LWA1DataFile(filename)
-    
-    jd = astro.unix_to_utcjd(idf.getInfo('tStart'))
-    date = str(ephem.Date(jd - astro.DJD_OFFSET))
-    sampleRate = idf.getInfo('sampleRate')
-    nInts = idf.getInfo('nFrames') / (30000 * len(antennas) / 2)
+    if not isinstance(idf, TBWFile):
+        raise RuntimeError("File '%s' does not appear to be a valid TBW file" % os.path.basename(filename))
+        
+    jd = idf.get_info('start_time').jd
+    date = idf.get_info('start_time').datetime
+    sample_rate = idf.get_info('sample_rate')
+    nInts = idf.get_info('nframe') // (30000 * len(antennas) // 2)
     
     # Get valid stands for both polarizations
     goodX = []
     goodY = []
-    for i in xrange(len(antennas)):
+    for i in range(len(antennas)):
         ant = antennas[i]
-        if ant.getStatus() < 22 and not config['all']:
-            pass
-        elif ant.stand.id in (35, 256, 257, 258, 259):
+        if ant.combined_status != 33 and not args.all:
             pass
         else:
             if ant.pol == 0:
@@ -253,37 +178,65 @@ def main(args):
                 
     # Report on the valid stands found.  This is a little verbose,
     # but nice to see.
-    print "Found %i good stands to use" % (len(good)/2,)
+    print("Found %i good stands to use" % (len(good)//2,))
     for i in good:
-        print "%3i, %i" % (antennas[i].stand.id, antennas[i].pol)
+        print("%3i, %i" % (antennas[i].stand.id, antennas[i].pol))
         
     # Number of frames to read in at once and average
     nFrames = 30000
-    nSets = idf.getInfo('nFrames') / (30000*len(antennas)/2)
+    nSets = idf.get_info('nframe') // (30000*len(antennas)//2)
     
-    print "Data type:  %s" % type(idf)
-    print "Captures in file: %i (%.3f s)" % (nInts, nInts*30000*400/sampleRate)
-    print "=="
-    print "Station: %s" % station.name
-    print "Date observed: %s" % date
-    print "Julian day: %.5f" % jd
-    print "Integration Time: %.3f s" % (400*nFrames/sampleRate)
-    print "Number of integrations in file: %i" % nSets
-    print "=="
+    print("Data type:  %s" % type(idf))
+    print("Captures in file: %i (%.3f s)" % (nInts, nInts*30000*400/sample_rate))
+    print("==")
+    print("Station: %s" % station.name)
+    print("Date observed: %s" % date)
+    print("Julian day: %.5f" % jd)
+    print("Integration Time: %.3f s" % (400*nFrames/sample_rate))
+    print("Number of integrations in file: %i" % nSets)
+    print("==")
     
-    if config['samples'] > nSets:
-        config['samples'] = nSets
-        
-    leftToDo = config['samples']
     basename = os.path.split(filename)[1]
     basename, ext = os.path.splitext(basename)
     
-    fitsFilename = "%s.ms" % basename
-    processChunk(idf, station, good, fitsFilename, LFFT=config['LFFT'], Overlap=1, pols=config['products'])
+    if args.casa:
+        fitsFilename = "%s.ms_1" % (basename,)
+    else:
+        fitsFilename = "%s.FITS_1" % (basename,)
+        
+    process_chunk(idf, station, good, fitsFilename, LFFT=args.fft_length, overlap=1, pfb=args.pfb, pols=args.products)
     
     idf.close()
 
 
 if __name__ == "__main__":
-    numpy.seterr(all='ignore')
-    main(sys.argv[1:])
+    parser = argparse.ArgumentParser(
+        description='cross-correlate data in a TBW file', 
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+    parser.add_argument('filename', type=str, 
+                        help='filename to correlate')
+    parser.add_argument('-m', '--metadata', type=str, 
+                        help='name of SSMIF or metadata tarball file to use for mappings')
+    parser.add_argument('-l', '--fft-length', type=aph.positive_int, default=16, 
+                        help='set FFT length')
+    parser.add_argument('-p', '--pfb', action='store_true', 
+                        help='enabled the PFB on the F-engine')
+    parser.add_argument('-q', '--quiet', dest='verbose', action='store_false', 
+                        help='run %(prog)s in silent mode')
+    parser.add_argument('-a', '--all', action='store_true', 
+                        help='correlated all dipoles regardless of their status')
+    pgroup = parser.add_mutually_exclusive_group(required=True)
+    pgroup.add_argument('-x', '--xx', dest='products', action='store_const', const=['xx',], 
+                        help='compute only the XX polarization product')
+    pgroup.add_argument('-y', '--yy', dest='products', action='store_const', const=['yy',], 
+                        help='compute only the YY polarization product')
+    pgroup.add_argument('-2', '--two-products', dest='products', action='store_const', const=['xx','yy'], 
+                        help='compute only the XX and YY polarization products')
+    pgroup.add_argument('-4', '--four-products', dest='products', action='store_const', const=['xx','yy','xy','yx'], 
+                        help='compute the XX, XY, YX, and YY polarization products')
+    parser.add_argument('--casa', action='store_true',
+                        help='write out measurement sets instead of FITS-IDI files')
+    args = parser.parse_args()
+    main(args)
+    
