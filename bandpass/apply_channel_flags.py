385cbd0d9c84bef8e225388698147c549e50ccd3
jaycedowell <jaycedowell@gmail.com>
2023-01-19 13:24:11 -0700

    CASA script to write the channel flags to the measurment set.

---
+++ bandpass/apply_channel_flags.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env casa
+
+import os
+import sys
+
+args = sys.argv[1:]
+if args[0][-4:] == '.txt':
+    ## File list - load and replace args
+    with open(args[0], 'r') as fh:
+        filelist = fh.read()
+    args = filelist.split('\n')
+    if args[-1] == '':
+        args = args[:-1]
+        
+for filename in args:
+    if filename[-1] == '/':
+        filename = filename[:-1]
+    flagname = os.path.basename(filename)
+    flagname, _ = os.path.splitext(flagname)
+    flagname = flagname+'_channel_flags.txt'
+    if not os.path.exists(flagname):
+        print("No channel flags for %s, skipping" % filename)
+        continue
+        
+    with open(flagname, 'r') as fh:
+        flagcmd = fh.read()
+    flagcmd = flagcmd.split(',')
+    flagcmd = ["0:%s" % f for f in flagcmd]
+    flagcmd = ','.join(flagcmd)
+    flagdata(vis=filename, mode='manual', spw=flagcmd)
