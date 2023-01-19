bf52444ebdb5949d7911c0a8f421592ae4b26a80
jaycedowell <jaycedowell@gmail.com>
2023-01-19 11:54:19 -0700

    Use the updated SSMIF.

--- correlate/process_tbw.sh
+++ correlate/process_tbw.sh
@@ -8,4 +8,4 @@ SCRIPT_PATH=`realpath ${0} | xargs dirname`
 #  -l 3920 to get 25 kHz wide channels
 #  -2 for only XX and YY
 #  --casa for measurement sets
-python3 ${SCRIPT_PATH}/correlateTBW.py -m ${SCRIPT_PATH}/SSMIF_181022.txt -p -l 3920 -2 --casa ${1}
+python3 ${SCRIPT_PATH}/correlateTBW.py -m ${SCRIPT_PATH}/SSMIF_NEW.txt -p -l 3920 -2 --casa ${1}
