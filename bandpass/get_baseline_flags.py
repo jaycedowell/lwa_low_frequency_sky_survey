745c5962a84a8e57920fc48a2bccba51d44f1d52
jaycedowell <jaycedowell@gmail.com>
2023-01-20 10:23:04 -0700

    Write an output file.

--- bandpass/get_baseline_flags.py
+++ bandpass/get_baseline_flags.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
+"""
+Given a LWA1 SSMIF file, find all signals on the same output RJ45 connector and
+create a flag file containing those baselines.
+"""
+
 import os
 import sys
 from lsl.common.stations import parse_ssmif
 
 
 def main(args):
+    # Load the SSMIF
     ssmifname = args[0]
     station = parse_ssmif(ssmifname)
     nant = len(station.antennas) // 2
     
+    # Find bad baselines and save by name
     bad = []
     for i in range(nant):
         a = station.antennas[2*i]
@@ -19,8 +26,12 @@ def main(args):
                 continue
                 
             if (a.arx.channel-1)//4 == (b.arx.channel-1)//4:
-                bad.append([f"LWA{a.stand.id:03d}", f"LWA{b.stand.id:03d}"])
-    print(bad)
+                bad.append(f"LWA{a.stand.id:03d}~LWA{b.stand.id:03d}")
+                
+    # Write to disk
+    with open('baseline_flags.txt', 'r') as fh:
+        fh.write(','.join(bad))
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
