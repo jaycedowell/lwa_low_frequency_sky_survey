312e7349c04af0655ec9a3af65e786484db2d669
jaycedowell <jayce.dowell@gmail.com>
2023-01-21 12:45:37 -0700

    Initial bandpass correction script.

---
+++ bandpass/apply_bandpass_corrs.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import numpy as np
+from casacore.tables import table
+from scipy.interpolate import interp1d
+
+
+def main(args):
+    # Initial file type check
+    if args[0][-4:] == '.txt':
+        ## File list - load and replace args
+        with open(args[0], 'r') as fh:
+            filelist = fh.read()
+        args = filelist.split('\n')
+        if args[-1] == '':
+            args = args[:-1]
+            
+    # Load in the antenna independent corrections...
+    fee = np.loadtxt('mean_fee_gain.txt')
+    imf = np.loadtxt('mean_fee_ant_imf.txt')
+    arx = np.loadtxt('mean_arx_gain.txt')
+    
+    # And convert them to interpolators
+    fee = interp1d(fee[:,0], fee[:,1])
+    imf = interp1d(imf[:,0], imf[:,1])
+    arx = interp1d(arx[:,0], arx[:,1])
+    
+    # Load the data
+    for filename in args:
+        ## Load in the observation-based gain correction
+        corrname = os.path.basename(filename)
+        corrname, _ = corrname.splitext(corrname)
+        corrname += '_gain_corr.txt'
+        if os.path.exists(corrname):
+            with open(corrname, 'r') as fh:
+                corr = float(fh.read())
+        else:
+            print("No temperature-based gain correction for %s" % filename)
+            corr = 1.0
+            
+        ## Get the frequency range of the file
+        tb = table(os.path.join(filename, 'SPECTRAL_WINDOW'), ack=False)
+        freq = np.array([], dtype=numpy.float64)
+        for freqIF in spw.col('CHAN_FREQ'):
+            freq = np.concatenate([freq, freqIF])
+        tb.close()
+        
+        ## Get the total per-channel correction for the data
+        corr = corr / fee(freq) / imf(freq) / arx(freq)
+        corr.shape = (1,)+corr.shape+(1,)
+        
+        ## Load in the actual data
+        tb = table(filename, readonly=False, ack=False)
+        data = tb.getcol('DATA')
+        data *= corr
+        tb.putcol('DATA', data)
+        tb.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
